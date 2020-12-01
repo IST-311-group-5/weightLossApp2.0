b7cc4504d84d14f29f6dac967459944ea874a908
daltonream <daltonream@gmail.com>
2020-12-01 10:04:26 -0500

    Hopefully fixed directory Issue and added comments on app.

--- Model/Usermodel.java
+++ Model/Usermodel.java
@@ -1,8 +1,9 @@
- /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * Weightloss App
+ * Group 5 
+ * IST 311
  */
+
 package Model;
 
 import java.io.Serializable;
--- controller/ProfileViewController.java
+++ controller/ProfileViewController.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Weightloss App
+ * Group 5 
+ * IST 311
  */
 package Controller;
 import Model.Usermodel;
