db825b72640e5e0125479dec7f0115802c38d18e
Liam-boston <60274837+Liam-boston@users.noreply.github.com>
2020-12-07 09:08:25 -0500

    Cleaned up & formatted

--- controller/LogInViewController.java
+++ controller/LogInViewController.java
@@ -24,13 +24,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 
-/**
- * FXML Controller class
- *
- */
 public class LogInViewController implements Initializable {
-    EntityManager manager;
-   
     @FXML
     private TextField userID;
 
@@ -40,120 +34,126 @@ public class LogInViewController implements Initializable {
     @FXML
     private Button createAccount;
 
-
     @FXML
-    private TextField userName;   
+    private TextField userName;
     
+    EntityManager manager;
 
     /**
-     * Initializes the controller class.
+     * 
+     * @param url
+     * @param rb 
      */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        manager = (EntityManager) Persistence.createEntityManagerFactory("WeightLossAppFXMLPU").createEntityManager();
+
+        assert userID != null : "fx:id=\"userID\" was not injected: check your FXML file 'logInView.fxml'.";
+        assert userName != null : "fx:id=\"userName\" was not injected: check your FXML file 'logInView.fxml'.";
+        assert logIn != null : "fx:id=\"logIn\" was not injected: check your FXML file 'logInView.fxml'.";
+        assert createAccount != null : "fx:id=\"createAccount\" was not injected: check your FXML file 'logInView.fxml'.";
+    }
     
+    /*******************************Button Operations************************************/
     
+    /**
+     * Opens a create account page
+     * 
+     * @param event
+     * @throws IOException 
+     */
     @FXML
-    void createAccount(ActionEvent event) throws IOException { //opens up a create account page
+    void createAccount(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/DetailedModelView.fxml"));
 
         Parent detailedModelView = loader.load();
-        
+
         Scene tableViewScene = new Scene(detailedModelView);
 
-            Stage stage = new Stage();
-            stage.setScene(tableViewScene);
+        Stage stage = new Stage();
+        stage.setScene(tableViewScene);
 
-            stage.show();
-            
+        stage.show();
+
         //--------------------Go back----------------------------
-        
         DetailedModelViewController detailedControlled = loader.getController();
 
         Scene currentScene = ((Node) event.getSource()).getScene();
         Stage login = (Stage) ((Node) event.getSource()).getScene().getWindow();
         detailedControlled.setPreviousScene(currentScene);
-          
-        
+
         stage.setScene(tableViewScene);
         stage.show();
         login.close();
-        
-
     }
-    
 
+    /**
+     * Logs in and then shows the profile page
+     * 
+     * @param event
+     * @throws IOException 
+     */
     @FXML
-    void logIn(ActionEvent event) throws IOException { // logs in and then shows the profile page
+    void logIn(ActionEvent event) throws IOException {
         String userid = userID.getText();
         String username = userName.getText();
-        
+
         if (userid.isEmpty() || username.isEmpty()) {
-            
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Not Valid Credentials");
             alert.setHeaderText("You did not fill in either the ID or Username");
             alert.setContentText("Please fill in the proper fields");
-            alert.showAndWait(); 
-            
+            alert.showAndWait();
         } else {
-
             int intUserID = Integer.parseInt(userid);
-            readByIDAndName(intUserID, username, event); 
-
+            readByIDAndName(intUserID, username, event);
         }
-
     }
-    public void readByIDAndName (int id, String name, ActionEvent event){ // pulls all the info from the db. 
+    
+    /**********************************Helper Methods**************************************/
+
+    /**
+     * Pulls all the info from the DB
+     * 
+     * @param id
+     * @param name
+     * @param event 
+     */
+    public void readByIDAndName(int id, String name, ActionEvent event) { 
         Query query = manager.createNamedQuery("Usermodel.readByNameAndId");
 
         try {
-            
-        query.setParameter("id", id);
-        query.setParameter("name", name);
-        
-        Usermodel user = (Usermodel) query.getSingleResult();
-        
-       
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/ProfileView.fxml"));
+            query.setParameter("id", id);
+            query.setParameter("name", name);
 
-        Parent detailedModelView = loader.load();
+            Usermodel user = (Usermodel) query.getSingleResult();
 
-        Scene tableViewScene = new Scene(detailedModelView);
-            
-        ProfileViewController profileControllerView = loader.getController();
-           
-        profileControllerView.initData(user);
-        
-        Stage logInView = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/ProfileView.fxml"));
+
+            Parent detailedModelView = loader.load();
+
+            Scene tableViewScene = new Scene(detailedModelView);
+
+            ProfileViewController profileControllerView = loader.getController();
+
+            profileControllerView.initData(user);
+
+            Stage logInView = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
             logInView.close();
 
             Stage stage = new Stage();
             stage.setScene(tableViewScene);
-            
+
             stage.show();
-           
-        
-
-
-         } catch (Exception ex) {
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
-            
+
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Not Valid Credentials");
             alert.setHeaderText("There is no account with that ID and/or Username");
             alert.setContentText("Please correct information or create an account.");
-            alert.showAndWait(); 
+            alert.showAndWait();
         }
-        
-        }
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        manager = (EntityManager) Persistence.createEntityManagerFactory("WeightLossAppFXMLPU").createEntityManager();
-        
-        assert userID != null : "fx:id=\"userID\" was not injected: check your FXML file 'logInView.fxml'.";
-        assert userName != null : "fx:id=\"userName\" was not injected: check your FXML file 'logInView.fxml'.";
-        assert logIn != null : "fx:id=\"logIn\" was not injected: check your FXML file 'logInView.fxml'.";
-        assert createAccount != null : "fx:id=\"createAccount\" was not injected: check your FXML file 'logInView.fxml'.";
-    }    
-    
+    }
 }
